6bf04b0b3a849167ae36f08bc54eef2c4ea915d2
KostayUa <kostay.ua@gmail.com>
2023-07-19 21:48:16 +0300

    Task1

--- src/app/Task1.java
+++ src/app/Task1.java
@@ -1,8 +1,22 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Task1 {
 
     public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Tom");
+        names.add("Amy");
+        names.add("John");
+        names.add("Tim");
+        names.add("Alex");
 
+        AtomicInteger count = new AtomicInteger(0);
+        names.stream()
+            .filter(str -> str.startsWith("T"))
+            .forEach(name -> System.out.println(count.incrementAndGet() + ") " + name));
     }
 }
